ff868a59f9d186510511288e242b69f13f5e0446
baga <barbara.gawalewicz@ailleron.com>
2017-09-21 18:21:19 +0200

    test

--- src/test/java/baga/SimpleJava8Test.java
+++ src/test/java/baga/SimpleJava8Test.java
@@ -2,10 +2,13 @@ package baga;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.springframework.util.StreamUtils;
 
 import java.math.BigDecimal;
 import java.util.*;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -70,6 +73,8 @@ public class SimpleJava8Test {
         myData.stream().findFirst().ifPresent(log::info);
         myData.forEach(item -> log.info("Item: " + item));
 
+        Stream dataStream;
+
         List<Employee> newResults = myEmployeeList.stream().map(employee -> {
             Employee object = new Employee();
 
@@ -89,6 +94,88 @@ public class SimpleJava8Test {
         newResults.forEach(employee -> {
             log.info("New Result: " + employee.toString());
         });
+
+
+        Stream.of(myData).map(Object::toString).reduce((a, b) -> a + " " + b).ifPresent(log::info);
+
+        List<String> list = new ArrayList<String>();
+        list.add("java");
+        list.add("php");
+        list.add("python");
+        list.add("perl");
+        list.add("c");
+        list.add("groovy");
+        list.add("javascript");
+        list.add("angularJS");
+        list.add("react");
+        list.add("ruby");
+        list.add("scale");
+        list.add("c#");
+
+
+        Stream<Integer> lengthStream = list.stream().map(String::length);
+        Optional<Integer> sum = lengthStream.reduce((x, y) -> x + y);
+        log.info("SUM1: " + sum.get());
+
+        int sum2 = list.stream().map(String::length).reduce(0, (x, y) -> x + y);
+        log.info("SUM2: " + sum2);
+
+        int sum3 = list.stream().map(String::length).reduce(Integer::sum).get();
+        log.info("SUM3: " + sum3);
+
+        List<String> item1 = list.stream().filter(element -> element.startsWith("p")).map(String::toUpperCase).collect(Collectors.toList());
+        log.info("ITEM1: " + item1);
+
+        String listOfElements = list.stream().peek(item -> log.debug("step1: " + item ))
+                                .sorted().peek(item -> log.debug("step2: " + item))
+                                .map(String::toUpperCase).peek(item -> log.debug("step3: " + item))
+                                .reduce((a, b) -> a + " - " + b).get();
+        log.info("String: " + listOfElements);
+
+
+        String[] myArray = list.stream().sorted().toArray(String[]::new);
+        List<String> myListSorted = list.stream().sorted().collect(Collectors.toList());
+
+        log.info("myArray: " + Arrays.toString(myArray));
+        log.info("myListSorted: " + myListSorted.toString());
+
+        log.info("Count: " + (long) list.size());
+
+        Stream<String> wordStream = list.stream();
+        List<Integer> values = wordStream.map(String::length).collect(Collectors.toList());
+        log.info("values: " + values);
+
+        List<String> myNewList = list.stream().map(a -> (a.startsWith("p") || a.startsWith("r") ? a + "*" : a + "!") ).collect(Collectors.toList());
+        log.info("mapEX: " + myNewList.toString());
+
+        List<String> sortedList = myNewList.stream().sorted().collect(Collectors.toList());
+        log.info("mapEX sorted: " + sortedList.toString());
+
+
+
+
+
+        Optional<Integer> s = list.stream().map(String::length).reduce((x, y) -> x + y);
+        System.out.println(s);
+
+        values.stream().reduce((a, b) -> a + b).ifPresent(result -> log.info("My sum: " + result));
+
+        Optional<Integer> test = values.stream().reduce(Integer::sum);
+
+
+//        int sum2 = lengthStream.reduce(0, (x, y) -> x + y);
+//        log.info("SUM2: " + sum2);
+
+        List<String> list1 = Arrays.asList("Test1", "Test2", "Test3");
+        List<String> list2 = Arrays.asList("Test4", "Test5", "Test6");
+
+        Stream<String> concatMy = Stream.concat(list1.stream(), list2.stream());
+
+        List<String> valuesAll = concatMy.collect(Collectors.toList());
+
+
+        log.info("List: " + valuesAll);
+
     }
 
     @Test
@@ -100,15 +187,15 @@ public class SimpleJava8Test {
         HOSTING.put(3, "digitalocean.com");
         HOSTING.put(4, "aws.amazon.com");
 
-//        //Map -> Stream -> Filter -> String
-//        String result = HOSTING.entrySet().stream().filter(item -> "aws.amazon.com".equals(item.getValue())).map(Map.Entry::getValue).collect(Collectors.joining());
-//
-//        // Map<Integer, String> collect = HOSTING.entrySet().stream().filter(item -> item.getKey() == 2).collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
-//        Map<Integer, String> collect = HOSTING.entrySet().stream().filter(item -> item.getKey() == 2).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-//        log.info("Result - String: " + result);
-//        log.info("Result - Collect: " + collect.toString());
-//
-//
+        //Map -> Stream -> Filter -> String
+        String result = HOSTING.entrySet().stream().filter(item -> "aws.amazon.com".equals(item.getValue())).map(Map.Entry::getValue).collect(Collectors.joining());
+
+        // Map<Integer, String> collect = HOSTING.entrySet().stream().filter(item -> item.getKey() == 2).collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
+        Map<Integer, String> collect = HOSTING.entrySet().stream().filter(item -> item.getKey() == 2).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        log.info("Result - String: " + result);
+        log.info("Result - Collect: " + collect.toString());
+
+
         SimpleJava8 streamTest = new SimpleJava8();
 
         streamTest.streamToMap(HOSTING);
@@ -118,7 +205,7 @@ public class SimpleJava8Test {
     @Test
     public void mapTestJava8List() {
 
-        String[] employees = new String[] {"Maciej Kowalski", "Daniel Kowalski", "Monika Kowalska", "Ewelina Kowalska", "Agnieszka Enigma"};
+        String[] employees = new String[]{"Maciej Kowalski", "Daniel Kowalski", "Monika Kowalska", "Ewelina Kowalska", "Agnieszka Enigma"};
 
 
         SimpleJava8 listTest = new SimpleJava8();
