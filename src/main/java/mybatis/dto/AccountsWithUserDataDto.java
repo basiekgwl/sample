fd6301bc00681bf60474dc81e88cb60d0a23c3b0
baga <barbara.gawalewicz@ailleron.com>
2017-09-25 23:56:11 +0200

    refactoring II - interfaces - draft

--- src/main/java/mybatis/dto/AccountsWithUserDataDto.java
+++ src/main/java/mybatis/dto/AccountsWithUserDataDto.java
@@ -12,24 +12,16 @@ public class AccountsWithUserDataDto extends AccountDto implements IAccountDto {
 
     private UserDto userDto;
 
-    public UserDto getUserDto() {
-        return userDto;
-    }
-
-    public void setUserDto(UserDto userDto) {
-        this.userDto = userDto;
-    }
-
     public AccountsWithUserDataDto returnAccountWithUserDto(UserAccountEntity userAccountEntity) {
 
-        UserDto userDto = new UserDto();
+        UserDto myNewUserDto = new UserDto();  //?????
 
         AccountsWithUserDataDto userDataDto = new AccountsWithUserDataDto();
 
         userDataDto.setNrb(userAccountEntity.getAccountNrb());
         userDataDto.setAccountType(userAccountEntity.getAccountType());
         userDataDto.setBalance(userAccountEntity.getBalance());
-        userDataDto.setUserDto(userDto.returnUserData(userAccountEntity.getUserEntity()));
+        userDataDto.setUserDto(myNewUserDto.returnUserData(userAccountEntity.getUserEntity()));
 
         return userDataDto;
     }
